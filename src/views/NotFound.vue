3aed2aa326267b8b867eaf89d86bf856d9b8331d
lapinp <nvzcsoft@yandex.ru>
2023-03-23 10:57:47 +0300

    fix(404 page): edit behavior

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,8 +2,18 @@
   <div class="not-found">404</div>
 </template>
 <script>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
 export default {
   name: "NotFound",
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      router.push("/main");
+    });
+  },
 };
 </script>
 <style lang="scss" scoped>
