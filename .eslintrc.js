f0d5b9afb8a1276a805355c698565b0893db7c92
Yedil A <akhmetkarimov.yedil@gmail.com>
2023-05-22 19:11:41 +0600

    config: eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
     root: true,
-
     env: {
         node: true,
     },
-
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
-
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
     parserOptions: {
         parser: '@babel/eslint-parser',
     },
-
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -21,7 +17,6 @@ module.exports = {
         'no-irregular-whitespace': 'off',
         curly: 'error',
     },
-
     overrides: [
         {
             files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    './src/**/*.js': ['npm run lint:code', 'npm run lint:code:fix', 'npm run test:unit'],
-    './src/**/*.vue': ['npm run lint:style'],
+    '*.js': ['npm run lint:code', 'npm run lint:code:fix', 'npm run test:unit'],
+    '*.vue': ['npm run lint:style'],
 };
