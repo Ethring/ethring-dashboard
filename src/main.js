82794b59176c89a05a73f5e2099e3691d44a6a28
Yedil A <akhmetkarimov.yedil@gmail.com>
2023-06-01 18:57:19 +0600

    fix: sentry init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import VueClickAway from 'vue3-click-away';
 import { vue3Debounce } from 'vue-debounce';
 
+import * as Sentry from '@sentry/vue';
+import { BrowserTracing } from '@sentry/browser';
+
 import App from './App.vue';
 
 import Router from './routes';
@@ -12,10 +15,27 @@ import '@/assets/styles/index.scss';
 
 import { i18n } from '@/shared/i18n';
 
-createApp(App)
+const app = createApp(App)
     .directive('debounce', vue3Debounce({ lock: true }))
     .use(store)
     .use(VueClickAway)
     .use(Router)
-    .use(i18n)
-    .mount('#app');
+    .use(i18n);
+
+if (process.env.VUE_APP_SENTRY_DSN) {
+    Sentry.init({
+        app,
+        dsn: process.env.VUE_APP_SENTRY_DSN,
+        tunnel: new URL(process.env.VUE_APP_SENTRY_DSN).origin + '/tunnel',
+        release: process.env.VUE_APP_RELEASE,
+        environment: process.env.NODE_ENV,
+        integrations: [
+            new BrowserTracing({
+                routingInstrumentation: Sentry.vueRouterInstrumentation(Router),
+            }),
+        ],
+        tracesSampleRate: 0.5,
+    });
+}
+
+app.mount('#app');
