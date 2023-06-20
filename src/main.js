43814cb1f3dfe8dd869a4244928b5a46d4b762ef
Yedil A <akhmetkarimov.yedil@gmail.com>
2023-06-20 12:09:46 +0000

    feat: sentry session set up

--- src/main.js
+++ src/main.js
@@ -36,10 +36,20 @@ getChainList().then((chains) => {
             tunnel: new URL(process.env.VUE_APP_SENTRY_DSN).origin + '/tunnel',
             release: process.env.VUE_APP_RELEASE,
             environment: process.env.NODE_ENV,
+
+            // This sets the sample rate to be 10%. You may want this to be 100% while
+            // in development and sample at a lower rate in production
+            replaysSessionSampleRate: 0.1,
+
+            // If the entire session is not sampled, use the below sample rate to sample
+            // sessions when an error occurs.
+            replaysOnErrorSampleRate: 1.0,
+
             integrations: [
                 new BrowserTracing({
                     routingInstrumentation: Sentry.vueRouterInstrumentation(Router),
                 }),
+                new Sentry.Replay(),
             ],
             tracesSampleRate: 0.5,
         });
