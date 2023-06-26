aa9c089627e9b783e90f9f8189f5ee5ef52f1898
unixhelloworld <akhmetkarimov.yedil@gmail.com>
2023-06-26 14:00:19 +0600

    fix: logger removed

--- src/shared/utils/request.js
+++ src/shared/utils/request.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
-import * as AxiosLogger from 'axios-logger';
 import adapter from 'axios/lib/adapters/xhr';
 
 const HttpRequest = axios.create({
@@ -8,7 +7,5 @@ const HttpRequest = axios.create({
     headers: { 'Cache-Control': 'no-cache' },
     adapter: cacheAdapterEnhancer(adapter),
 });
-
-HttpRequest.interceptors.request.use(AxiosLogger.requestLogger);
 
 export default HttpRequest;
