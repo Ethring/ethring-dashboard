8bf316ba44d576d71626b5b4c7c31b006f4eb800
unixhelloworld <akhmetkarimov.yedil@gmail.com>
2023-06-26 13:59:32 +0600

    feat: axios logger

--- src/shared/utils/request.js
+++ src/shared/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
+import * as AxiosLogger from 'axios-logger';
 import adapter from 'axios/lib/adapters/xhr';
 
 const HttpRequest = axios.create({
@@ -7,5 +8,7 @@ const HttpRequest = axios.create({
     headers: { 'Cache-Control': 'no-cache' },
     adapter: cacheAdapterEnhancer(adapter),
 });
+
+HttpRequest.interceptors.request.use(AxiosLogger.requestLogger);
 
 export default HttpRequest;
