8ad70cfedf6e778082384981684e0b3de1535689
unixhelloworld <akhmetkarimov.yedil@gmail.com>
2023-06-26 14:14:40 +0600

    feat: cache for 3 hours

--- src/shared/utils/request.js
+++ src/shared/utils/request.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
 import adapter from 'axios/lib/adapters/xhr';
 
+const CACHE_TIME = 3 * 60 * 60 * 1000; // 3hrs * 60min * 60sec * 1000ms = 3hrs
+
 const HttpRequest = axios.create({
     baseURL: '/',
-    headers: { 'Cache-Control': 'no-cache' },
+    headers: { 'Cache-Control': `max-age=${CACHE_TIME}` },
     adapter: cacheAdapterEnhancer(adapter),
 });
 
