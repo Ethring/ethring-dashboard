f42e49c0be3f90fe01d0b098ae1854f50a1eb6e2
Egorrrka1613 <reutov622@gmail.com>
2023-08-16 13:04:44 +0400

    fix: add rus lock in test

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         trace: 'on-first-retry',
         testIdAttribute: 'data-qa',
         screenshot: 'only-on-failure',
+        locale: 'ru-RU',
     },
 
     webServer: {
