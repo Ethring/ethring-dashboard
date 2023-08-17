b71a7ed3bc85796e8ee7579420d52569f7ba59b4
Egorrrka1613 <reutov622@gmail.com>
2023-08-18 00:12:06 +0400

    fix: log mock file

--- tests/mockServer/mockServer.js
+++ tests/mockServer/mockServer.js
@@ -1,4 +1,9 @@
 var mockserver = require('mockserver-node');
+const path = require('path');
+const fs = require('fs');
+
+const pathToMock = path.resolve(__dirname, 'initializer.json');
+console.log(`>>> Path to mockfile:\n${pathToMock}\nis exist: ${fs.existsSync(pathToMock)}`);
 
 mockserver.start_mockserver({
     serverPort: 1080,
@@ -10,7 +15,7 @@ mockserver.start_mockserver({
         '-Dmockserver.corsAllowHeaders="Allow, Content-Encoding, Content-Length, Content-Type, ETag, Expires, Last-Modified, Location, Server, Vary, Authorization"',
         '-Dmockserver.corsAllowCredentials=true',
         '-Dmockserver.corsMaxAgeInSeconds=300',
-        '-Dmockserver.initializationJsonPath="/Users/egorreutov/work/zomet-dashboard/tests/mockServer/initializer.json"',
+        `-Dmockserver.initializationJsonPath=${pathToMock}`,
         '-Dmockserver.disableLogging=true',
         '-Dmockserver.maxLogEntries=20',
         '-Dmockserver.logLevel="OFF"',
