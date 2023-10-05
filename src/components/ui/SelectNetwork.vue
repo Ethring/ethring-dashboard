3a9cd2984c72816a4195b9017ba8ed2a1a3f6949
baratmaira <mairabarat@citadel.one>
2023-10-05 20:18:53 +0600

    fix: rm TokenIcon bgColor

--- src/components/ui/SelectNetwork.vue
+++ src/components/ui/SelectNetwork.vue
@@ -129,12 +129,11 @@ export default {
             display: flex;
             justify-content: center;
             align-items: center;
+            
             width: 40px;
             height: 40px;
             min-width: 40px;
-            border-radius: 50%;
-
-            background: var(--#{$prefix}primary);
+        
             margin-right: 10px;
 
             svg {
