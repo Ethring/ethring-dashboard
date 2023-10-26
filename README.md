1e2fa2363155bbf6b2df598fd3d8ad43f3496a87
Egorrrka1613 <reutov622@gmail.com>
2023-10-26 14:49:04 +0400

    test: add screen test by all page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Hello Citadel.dashboard!
+
+If you want update snapshot, you need run this code in work dir:
+`docker run --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v1.39.0-jammy /bin/bash`
+`npm i`
+`xvfb-run npx playwright test --update-snapshots`
--- tests/e2e/dashboard.spec.ts
+++ tests/e2e/dashboard.spec.ts
@@ -11,5 +11,17 @@ test.describe('Dashboard page tests', () => {
     test('Case#1: Auth by MM, empty wallet', async ({ browser, context, page: Page, dashboardEmptyWallet }) => {
         await dashboardEmptyWallet.page.waitForSelector('div.ant-skeleton-active', { state: 'detached', timeout: 60000 });
         await expect(dashboardEmptyWallet.page).toHaveScreenshot();
+
+        const sendPage = await dashboardEmptyWallet.goToSend();
+        await expect(sendPage.page).toHaveScreenshot();
+
+        const swapPage = await sendPage.goToSwap();
+        await expect(swapPage.page).toHaveScreenshot();
+
+        const bridgePage = await swapPage.goToBridge();
+        await expect(bridgePage.page).toHaveScreenshot();
+
+        const superSwapPage = await swapPage.goToSuperSwap();
+        await expect(superSwapPage.page).toHaveScreenshot();
     });
 });
