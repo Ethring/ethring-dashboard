49cb266760fd6884948724aafe394065ecbaca22
baratmaira <mairabarat@citadel.one>
2023-11-27 12:24:19 +0600

    fix: ecosystems menu-item

--- src/Adapter/UI/Widgets/ConnectToEcosystems.vue
+++ src/Adapter/UI/Widgets/ConnectToEcosystems.vue
@@ -1,8 +1,8 @@
 <template lang="">
     <a-menu-item-group key="ecosystems">
-        <div :key="record.ecosystem" v-for="record in TO_CONNECT">
+        <a-menu-item  :key="record.ecosystem" v-for="record in TO_CONNECT">
             <ConnectTo :name="$t(record.name)" :connect="() => connect(record.ecosystem)" :logos="record.logos" />
-        </div>
+        </a-menu-item >
     </a-menu-item-group>
 </template>
 
