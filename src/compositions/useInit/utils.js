ae0f144ba1b66e4cac59a28e7c53cd4c87413ac3
Diana Maduan <maduandiana@gmail.com>
2023-11-28 18:57:12 +0600

    fix: osmo token size

--- src/compositions/useInit/utils.js
+++ src/compositions/useInit/utils.js
@@ -66,6 +66,10 @@ export const formatRecord = (record, { net, chain, logo }) => {
         record.id = `${net}:asset__native:${record.symbol}`;
     }
 
+    if (record.symbol === 'OSMO') {
+        record.logo = 'https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/osmo.png';
+    }
+
     return record;
 };
 
