812984c418514ebb141314f454eb6a80a6294761
baratmaira <mairabarat@citadel.one>
2023-11-30 19:40:23 +0600

    fix: updated snapshots

--- src/components/ui/ServiceIcon.vue
+++ src/components/ui/ServiceIcon.vue
@@ -26,18 +26,20 @@ export default {
 
 <style lang="scss" scoped>
 .service-icon {
-    cursor: default;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
+    @include pageFlexRow;
     justify-content: center;
+
     width: 24px;
     height: 24px;
+
+    cursor: default;
+
+    font-weight: 600;
     font-size: var(--#{$prefix}default-fs);
     color: var(--#{$prefix}primary-text);
+
     border-radius: 50%;
     background-color: var(--#{$prefix}primary-text);
-
     border: 1px solid rgba(0, 0, 0, 0.1);
 
     margin-right: 10px;
@@ -45,6 +47,7 @@ export default {
     img {
         width: 20px;
         height: 20px;
+        border-radius: 50%;
         object-fit: contain;
     }
 }
