06426539d78c335dcf69a96ab7b20919a18f822d
baratmaira <mairabarat@citadel.one>
2023-12-14 17:25:18 +0600

    fix: rm logs

--- src/App.vue
+++ src/App.vue
@@ -213,7 +213,6 @@ export default {
         watch(currentChainInfo, async () => await callSubscription());
 
         watch(walletAccount, async () => {
-            console.log(walletAccount, '--wallet account');
             store.dispatch('tokenOps/setSrcToken', null);
             store.dispatch('tokenOps/setDstToken', null);
 
