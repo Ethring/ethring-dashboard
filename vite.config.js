4e4cbc974072b3efaf907bc1d864b2e26858b986
baratmaira <mairabarat@citadel.one>
2024-01-31 12:26:06 +0600

    fix: leap icon

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { VitePWA } from 'vite-plugin-pwa';
