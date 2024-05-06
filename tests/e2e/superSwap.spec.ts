f15f128e16d127820910523c9ce6b6abff4ade1f
baratmaira <mairabarat@citadel.one>
2024-05-06 20:59:39 +0500

    test: updated superswap test

--- tests/e2e/superSwap.spec.ts
+++ tests/e2e/superSwap.spec.ts
@@ -136,6 +136,8 @@ testMetaMask.describe('SuperSwap e2e tests', () => {
 
         await sleep(FIVE_SECONDS);
 
+        await superSwapPage.page.locator(`//button[@data-qa="confirm"]`);
+
         await expect(superSwapPage.page).toHaveScreenshot();
     });
 });
