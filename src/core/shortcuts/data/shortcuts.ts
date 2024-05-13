b551077d2e5c66dc276f1d27e5dacdcb03bf3890
Yedil A <akhmetkarimov.yedil@gmail.com>
2024-05-13 18:04:36 +0500

    test: added test shortcut for checking stake estimate

--- src/core/shortcuts/data/shortcuts.ts
+++ src/core/shortcuts/data/shortcuts.ts
@@ -1,7 +1,7 @@
 export enum AvailableShortcuts {
     CitadelOneStake = 'SC-citadel-one-stake',
     AirdropFarmingPortfolio = 'airdrop-farming-portfolio',
-    // StakeToCitadelOne = 'SC-stake-to-citadel-one',
+    StakeToCitadelOne = 'SC-stake-to-citadel-one',
     SwapToAtomAndStars = 'SC-swap-to-atom-and-stars',
     MintCollectionWithTransfer = 'SC-mint-collection-with-transfer',
     SellEverything = 'sell-everything-in-one-click',
