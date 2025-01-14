a008b7dc345b4ad554aad2f5523299efd02dd77a
Yedil A <akhmetkarimov.yedil@gmail.com>
2025-01-14 18:42:32 +0500

    fix: disable mixpanel

--- src/app/modules/mixpanel/index.js
+++ src/app/modules/mixpanel/index.js
@@ -1,14 +1,15 @@
 import VueMixpanel from 'vue-mixpanel';
 
 export default function useMixpanel(app) {
-    if (!process.env.MIXPANEL_TOKEN) return;
+    return;
+    // if (!process.env.MIXPANEL_TOKEN) return;
 
-    return app.use(VueMixpanel, {
-        token: process.env.MIXPANEL_TOKEN,
-        config: {
-            debug: true,
-            track_pageview: true,
-            persistence: 'localStorage',
-        },
-    });
+    // return app.use(VueMixpanel, {
+    //     token: process.env.MIXPANEL_TOKEN,
+    //     config: {
+    //         debug: true,
+    //         track_pageview: true,
+    //         persistence: 'localStorage',
+    //     },
+    // });
 }
